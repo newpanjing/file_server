23b2809ec4e1054d6ee7f535378ea817037354c9
武松 <599194993@qq.com>
2024-03-28 14:33:40 +0800

    fix bug

--- main.go
+++ main.go
@@ -2,32 +2,30 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 func main() {
 	router := httprouter.New()
 	//获取当前目录
-
 	var host string
 
 	//解析命令行参数
 	flag.StringVar(&host, "host", "127.0.0.1:1234", "监听地址")
 	flag.Parse()
 
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	//获取当前路径
+	dir, _ := os.Getwd()
 	//设置静态文件目录
 	router.ServeFiles("/*filepath", http.Dir(dir))
+	fmt.Println(fmt.Sprintf("当前所在目录：%s", dir))
+
 	log.Printf("server start at http://%s", host)
-	err = http.ListenAndServe(host, router)
+	err := http.ListenAndServe(host, router)
 	if err != nil {
 		log.Fatal(err)
 	}
